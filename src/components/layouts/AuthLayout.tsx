a2652fb771cbdb900356dcf9d3ebe9497570ddfa
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2025-04-17 22:45:56 +0900

    design: 자잘한 스타일 수정

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -4,8 +4,8 @@ import logo from '@/assets/images/logo.png';
 
 const AuthLayout = () => {
   return (
-    <main className="mx-auto flex min-h-screen max-w-[600px] flex-col items-center bg-white p-4 pt-20">
-      <div className="mb-[80px] flex h-fit items-center gap-2">
+    <main className="mx-auto flex min-h-screen max-w-[600px] flex-col items-center bg-white p-4 pt-12">
+      <div className="mb-[48px] flex h-fit items-center gap-2">
         <img className="h-20 w-20" src={logo} alt="모두의 지갑 로고" />
         <p className="text-[36px] font-bold">모두의 지갑</p>
       </div>
