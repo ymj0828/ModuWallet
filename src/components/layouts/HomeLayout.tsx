7eefdbea430a9faa51888f5a050546450b804777
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2025-04-19 18:08:15 +0900

    feat: Header 컴포넌트 생성

--- src/components/layouts/HomeLayout.tsx
+++ src/components/layouts/HomeLayout.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from 'react-router-dom';
 
+import Header from '@/components/common/Header';
+
 const HomeLayout = () => {
   return (
-    <main className="mx-auto min-h-screen max-w-[600px] bg-white">
-      <Outlet />
-    </main>
+    <div className="mx-auto min-h-screen max-w-[600px] bg-white">
+      <Header />
+      <main className='p-4'>
+        <Outlet />
+      </main>
+    </div>
   );
 };
 
