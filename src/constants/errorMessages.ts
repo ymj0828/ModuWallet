d8017a56e43787d096e4224a84d28656ecda69d4
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2025-04-24 01:06:35 +0900

    feat: error message, placeholder 상수화

---
+++ src/constants/errorMessages.ts
@@ -0,0 +1,12 @@
+export const ERROR_MESSAGES = {
+  AUTH: {
+    REQUIRE_ID_DUPLICATE_CHECK: '아이디 중복 확인을 해주세요.',
+    REQUIRED_ID: '이름을 입력해주세요.',
+    ID_DUPLICATE: '이미 사용 중인 이름입니다.',
+    ID_LENGTH: '2자 이상 10자 이하로 입력해주세요.',
+    ID_INVALID_FORMAT: '한글, 영어, 숫자만 입력할 수 있어요.',
+    REQUIRED_PASSWORD: '비밀번호를 입력해주세요.',
+    REQUIRED_PASSWORD_CHECK: '비밀번호를 한 번 더 입력해주세요',
+    PASSWORD_NOT_MATCH: '비밀번호가 일치하지 않습니다.',
+  },
+};
